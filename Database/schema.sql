6747e1bb732ec878d045da59d621eed35fb7edd3
timothywang887 <timothywang887@gmail.com>
2026-09-02 19:35:33 -0700

    Started on Scriptable Objects
    Stopped cause I don't actually know we what we're trying to store still

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE `playerRank` (
-  `rankID` int PRIMARY KEY,
+  `rankId` int PRIMARY KEY,
   `rankName` varchar(20)
 );
 
 CREATE TABLE `castleRanks` (
-  `rankID` int PRIMARY KEY,
+  `rankId` int PRIMARY KEY,
   `rankName` varchar(20)
 );
 
 CREATE TABLE `tileTypes` (
-  `tileTypeID` int PRIMARY KEY,
+  `tileTypeId` int PRIMARY KEY,
   `tileName` varchar(50) UNIQUE
 );
 
 CREATE TABLE `player` (
-  `playerID` int PRIMARY KEY,
+  `playerId` int PRIMARY KEY,
   `username` varchar(25) UNIQUE NOT NULL,
   `pinHash` varchar(128) NOT NULL,
   `pinSalt` varcahr(128) NOT NULL,
@@ -22,23 +22,23 @@ CREATE TABLE `player` (
 );
 
 CREATE TABLE `castle` (
-  `castleID` int PRIMARY KEY,
-  `playerID` int UNIQUE NOT NULL,
+  `castleId` int PRIMARY KEY,
+  `playerId` int UNIQUE NOT NULL,
   `rank` int NOT NULL
 );
 
 CREATE TABLE `tile` (
-  `tileID` int PRIMARY KEY,
+  `tileId` int PRIMARY KEY,
   `type` int NOT NULL,
-  `castleID` int NOT NULL
+  `castleId` int NOT NULL
 );
 
-ALTER TABLE `player` ADD FOREIGN KEY (`rank`) REFERENCES `playerRank` (`rankID`);
+ALTER TABLE `player` ADD FOREIGN KEY (`rank`) REFERENCES `playerRank` (`rankId`);
 
-ALTER TABLE `castle` ADD FOREIGN KEY (`playerID`) REFERENCES `player` (`playerID`);
+ALTER TABLE `castle` ADD FOREIGN KEY (`playerId`) REFERENCES `player` (`playerId`);
 
-ALTER TABLE `castle` ADD FOREIGN KEY (`rank`) REFERENCES `castleRanks` (`rankID`);
+ALTER TABLE `castle` ADD FOREIGN KEY (`rank`) REFERENCES `castleRanks` (`rankId`);
 
-ALTER TABLE `tile` ADD FOREIGN KEY (`type`) REFERENCES `tileTypes` (`tileTypeID`);
+ALTER TABLE `tile` ADD FOREIGN KEY (`type`) REFERENCES `tileTypes` (`tileTypeId`);
 
-ALTER TABLE `tile` ADD FOREIGN KEY (`castleID`) REFERENCES `castle` (`castleID`);
+ALTER TABLE `tile` ADD FOREIGN KEY (`castleId`) REFERENCES `castle` (`castleId`);
