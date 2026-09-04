50db89858eeb03b42e859f5d20497c2932207262
Andrew Platosh <andrew@platosh.com>
2026-09-04 13:54:07 -0700

    Added test debug statement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Start is called once before the first frame update
     void Start()
     {
-        
+        Debug.Log("Test");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+  
     }
 }
