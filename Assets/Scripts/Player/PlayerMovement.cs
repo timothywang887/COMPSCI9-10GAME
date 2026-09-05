c81a42a233756c5cb023f9f0e376aab76cc606c6
Johnathan Pennock <jpennock5701@student.pps.net>
2026-09-04 23:09:29 -0700

    Basic Player Movement
    Uses fixed update.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first frame update
+    public Rigidbody rb;
+    public Vector2 input;
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public bool isGrounded = false;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.2f;
+
+    private Vector3 moveVelocity;
+
     void Start()
     {
-        Debug.Log("Test");
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-  
+        // 1. Gather input
+        input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+        // 2. Calculate movement direction relative to the player's local orientation
+        Vector3 moveDir = (transform.forward * input.y) + (transform.right * input.x);
+        
+        // 3. Set target velocity (leaving Y untouched so gravity still works)
+        moveVelocity = new Vector3(moveDir.x * moveSpeed, rb.linearVelocity.y, moveDir.z * moveSpeed);
+
+        // 4. Check ground status
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, groundLayer);
+
+        // 5. Check for Jump input
+        if (isGrounded && Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // 6. Apply the velocity consistently in FixedUpdate
+        // Note: Do NOT multiply by Time.deltaTime when setting velocity directly
+        rb.linearVelocity = new Vector3(moveVelocity.x, rb.linearVelocity.y, moveVelocity.z);
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
     }
 }
