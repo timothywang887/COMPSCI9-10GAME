0010e3a8a8c3408380d34d7426f6be77e982dfda
Krispy Kiwi <srawnsley7044@student.pps.net>
2026-09-15 13:13:59 -0700

    added an enemy

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float Health = 100f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5f);
+            print("space");
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        Health -= damage;
+        print(Health);
+        if (Health <= 0)
+        {
+            Die();
+        }
+    }
+
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
