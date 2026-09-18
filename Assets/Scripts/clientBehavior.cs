2bc6fcd355b050a799d6d0d9234668ced1041af0
timothywang887 <timothywang887@gmail.com>
2026-09-17 21:08:16 -0700

    After some thought, SPAN seems better - clientBehavior has thus been simplified

--- Assets/Scripts/clientBehavior.cs
+++ Assets/Scripts/clientBehavior.cs
@@ -2,86 +2,60 @@ using UnityEngine;
 using ValveSockets;
 using System;
 using System.Text;
+
 public class clientBehavior : MonoBehaviour
 {
     uint connection = 0;
     Address address = new Address();
-
+    NetworkingSockets client = new NetworkingSockets();
     int port = 67; // placeholder for port number
+    StatusCallback statusCallback;
+    MessageCallback messageCallback;
+
     void Start()
     {
-        utils.SetStatusCallback(status);
-        address.SetAddress("10.230.41.9"/*I think this is the correct IP address, unsure, espcially if it may change*/, port);
+        statusCallback = (ref StatusInfo info) =>
+        {
+            switch (info.connectionInfo.state)
+            {
+                case ConnectionState.None:
+                    break;
+
+                case ConnectionState.Connected:
+                    Debug.Log("Client connected to server - ID: " + connection);
+                    break;
+
+                case ConnectionState.ClosedByPeer:
+                case ConnectionState.ProblemDetectedLocally:
+                    client.CloseConnection(connection);
+                    Debug.Log("Client disconnected from server");
+                    break;
+            }
+        };
+
+        utils.SetStatusCallback(statusCallback);
+
+        // Unsure about the ip(it can change) or the port.
+        address.SetAddress("10.230.41.9", port);
         connection = client.Connect(ref address);
+
+        messageCallback = (in NetworkingMessage netMessage) =>
+        {
+            Debug.Log("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
+        };
     }
 
     void Update()
     {
-        if (VALVESOCKETS_SPAN)
-        {
-            MessageCallback message = (in NetworkingMessage netMessage) =>{
-		        Console.WriteLine("Message received from server - Channel ID: " + netMessage.channel + ", Data length: " + netMessage.length);
-            };
-        }
-        else
-        {
-            const int maxMessages = 20;
+        client.RunCallbacks();
 
-            NetworkingMessage[] netMessages = new NetworkingMessage[maxMessages];
-        }
+        client.ReceiveMessagesOnConnection(connection, messageCallback, 20);
 
-        if(!Console.KeyAvailable)
-        {
-            client.RunCallbacks();
-
-            if (VALVESOCKETS_SPAN)
-            {
-                client.ReceiveMessagesOnConnection(connection, message,20);
-            }
-            else
-            {
-                int netMessagesCount = client.ReceiveMessagesOnConnection(connection, netMessages, maxMessages);
-
-                if(netMessagesCount > 0)
-                {
-                    for (int i = 0; i < netMessagesCount; i++)
-                    {
-                        Console.WriteLine("Message received from server - Channel ID: " + netMessages.channel + ", Data length: " + netMessages.length);
-
-                        netMessage.Destroy();
-                    }
-                }
-            }
-        }
-        
     }
-    NetworkingSockets client = new NetworkingSockets();
 
-
-
-    StatusCallback status = (ref StatusInfo info) => {
-        switch (info.connectionInfo.state) {
-            case ConnectionState.None:
-                break;
-
-            case ConnectionState.Connected:
-                Console.WriteLine("Client connected to server - ID: " + connection);
-                break;
-
-            case ConnectionState.ClosedByPeer:
-            case ConnectionState.ProblemDetectedLocally:
-                client.CloseConnection(connection);
-                Console.WriteLine("Client disconnected from server");
-                break;
-        }
-    };
-
-    private void sendMessage(string message)
+    private void SendMessage(string message)
     {
         byte[] messageBytes = Encoding.UTF8.GetBytes(message);
-        sockets.SendMessageToConnection(connection, messageBytes);
+        client.SendMessageToConnection(connection, messageBytes);
     }
-
-
-
 }
