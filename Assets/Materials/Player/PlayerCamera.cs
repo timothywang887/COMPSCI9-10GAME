47ba3a367a9526d52e80760536857970f214b004
Karol <kiwanow3043@student.pps.net>
2026-09-25 13:07:57 -0700

    Added enemy spawning

--- Assets/Materials/Player/PlayerCamera.cs
+++ Assets/Materials/Player/PlayerCamera.cs
@@ -111,7 +111,7 @@ public class PlayerCamera : MonoBehaviour
             // 4. Handle Mouse Turning Z-Roll (Mouse roll combined with movement roll)
             float targetMouseRoll = Mathf.Clamp((-smoothTurnVelocity) * 75f, -200f, 200f) * mouseTiltFactor;
             float rollLerpSpeed = 8f;
-            Debug.Log(Mathf.Exp(-rollLerpSpeed * Time.deltaTime));
+            //Debug.Log(Mathf.Exp(-rollLerpSpeed * Time.deltaTime));
             float rollDecayFactor = 1f - Mathf.Exp(-rollLerpSpeed * Time.deltaTime);
             currentZRoll = Mathf.Lerp(currentZRoll, targetMouseRoll, rollDecayFactor);
 
